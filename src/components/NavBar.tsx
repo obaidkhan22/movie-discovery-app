e23d6ef4b454670a9b62d1ddeef51dc0d4b998ab
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-02-21 01:56:25 +0430

    fix navbar.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,8 +4,8 @@ import DarkModeSwitch from "./DarkModeSwitch";
 const NavBar = () => {
   return (
     <nav>
-      <HStack justifyContent="space-between">
-        <Image src={logo} boxSize={20} />
+      <HStack justifyContent="space-between" padding={2}>
+        <Image src={logo} boxSize="60px" />
         <Text>Search Movies...</Text>
         <DarkModeSwitch />
         <Text>User Account</Text>
