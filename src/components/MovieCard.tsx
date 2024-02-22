ed9b5c8068bbe85e17b2e1c169a377c592bc9108
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-02-23 01:33:53 +0430

    fix movieCard's borderRadius

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,20 +1,17 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Movie } from "../entities/FetchResponseMovies";
 import getImageURL from "../services/getImageURL";
-import MovieCardContainer from "./MovieCardContainer";
 interface Props {
   movie: Movie;
 }
 const MovieCard = ({ movie }: Props) => {
   return (
-    <MovieCardContainer>
-      <Card>
-        <Image src={getImageURL(movie.poster_path)} />
-        <CardBody>
-          <Heading fontSize={24}>{movie.title}</Heading>
-        </CardBody>
-      </Card>
-    </MovieCardContainer>
+    <Card borderRadius={10} overflow={"hidden"}>
+      <Image src={getImageURL(movie.poster_path)} />
+      <CardBody>
+        <Heading fontSize={20}>{movie.title}</Heading>
+      </CardBody>
+    </Card>
   );
 };
 
