6a60706bff48ea9e6ef0f60201021f8984824a5e
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-02-23 20:16:05 +0430

    shipping genres as static data.

--- src/components/GenresList.tsx
+++ src/components/GenresList.tsx
@@ -1,33 +1,20 @@
-import {
-  HStack,
-  Heading,
-  Image,
-  List,
-  ListItem,
-  Spinner,
-} from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import { HStack, Heading, Image, List, ListItem } from "@chakra-ui/react";
 
+import genres from "../data/genres";
 const GenresList = () => {
-  const { data, error, isLoading } = useGenres();
-
-  if (isLoading) return <Spinner />;
-  if (error) return null;
-
   return (
     <>
       <Heading fontSize={24}>Genres</Heading>
       <List>
-        {data?.genres.map((genre) => (
-          <HStack>
+        {genres.genres.map((genre) => (
+          <HStack marginY={2}>
             <Image
+              src={genre.image_background}
               boxSize={10}
-              marginY={1}
               borderRadius={10}
               objectFit="cover"
-              src="https://static.vecteezy.com/system/resources/thumbnails/028/274/915/small/strong-athletic-male-fighter-view-from-the-back-photo.jpg"
             />
-            <ListItem marginY="6px" fontSize="17px" key={genre.id}>
+            <ListItem fontSize="17px" key={genre.id}>
               {genre.name}
             </ListItem>
           </HStack>
