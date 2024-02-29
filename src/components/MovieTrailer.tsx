3366a511c32680048539204f6f4993c1be676fc7
Obaidullah Totakhail <obaidhamdard69@gmail.com>
2024-03-01 01:15:33 +0430

    fix the key property for movie's trailer.

--- src/components/MovieTrailer.tsx
+++ src/components/MovieTrailer.tsx
@@ -5,7 +5,6 @@ import { AspectRatio, HStack } from "@chakra-ui/react";
 const MovieTrailer = () => {
   const { id } = useParams();
   const { data } = useMovieTrailer(id!);
-  const url = `https://www.youtube.com/embed/${data?.results[0].key}`;
   return (
     <HStack
       justifyContent="center"
@@ -14,7 +13,12 @@ const MovieTrailer = () => {
       borderRadius={20}
     >
       <AspectRatio width="90%">
-        <iframe src={url} allowFullScreen />
+        <iframe
+          src={`https://www.youtube.com/embed/${
+            data?.results[0] ? data.results[0].key : ""
+          }`}
+          allowFullScreen
+        />
       </AspectRatio>
     </HStack>
   );
